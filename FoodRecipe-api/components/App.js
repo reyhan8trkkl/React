b6c76b28494103c6934a4d6fedca6360b8c36575
Reyhan Türkkal <94196332+reyhan8trkkl@users.noreply.github.com>
2022-02-21 20:41:22 +0300

    Add files via upload

---
+++ FoodRecipe-api/components/App.js
@@ -0,0 +1,68 @@
+import axios from "axios";
+import React, { useState } from "react";
+import Alert from "./Alert";
+import Recipe from "./Recipe";
+
+
+const App = () => {
+  const [query, setQery] = useState("");
+  const [recipes, setRecipes] = useState([]);
+  const [alert,setAlert] = useState("")
+
+  const appid = "14b435b5";
+  const appkey = "cfd3ad6305be97d6191611035d85b1c6";
+  const url = `https://api.edamam.com/search?q=${query}&app_id=${appid}&app_key=${appkey}`;
+
+  const getData = async () => {
+    if(query !==""){
+      const result = await axios.get(url, {
+        params: {
+          q: "chicken",
+          app_key: appid,
+          app_id: appkey,
+          origin: "*",
+          format: "json",
+        },
+      });
+      if(!result.data.more){
+       return setAlert("No food with such a name")
+      }
+      setRecipes(result.data.hits);
+      console.log(result);
+      setAlert("")
+      setQery("");
+    }else{
+      setAlert("Fill the form!")
+    }
+    }
+   
+  const onSub = (e) => {
+    e.preventDefault();
+    getData();
+  };
+  const onChange = (e) => {
+    setQery(e.target.value);
+  };
+
+  return (
+    <div className="App">
+      <h1 onClick={getData}>Food Searching App</h1>
+      <form className="search-form" onSubmit={onSub}>
+        { alert !== "" && <Alert alert={alert} />}
+        <input 
+          type="text"
+          placeholder="Search Food..."
+          autoComplete="off"
+          onChange={onChange}
+          value={query}
+        />
+        <input type="submit" value="search" />
+      </form>
+      <div className="recipes">
+        {recipes !== [] && recipes.map((recipe,i) => <Recipe key={i} recipe={recipe} />)}
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ FoodRecipe-api/components/RecipeDetails.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const RecipeDetails = ({ ingredients }) => {
+ 
+
+  return ingredients.map((ingredient, i) => {
+    return (
+      <ul className="ingredient-list">
+        <li className="ingredient-text">{ingredient.text}</li>
+        <li className="ingredient-weight">Weight - {ingredient.weight}</li>
+      </ul>
+    );
+  });
+};
+
+export default RecipeDetails;
